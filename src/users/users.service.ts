d8e498bbbff8aaaeaf9fcf31776b90cee7221812
heepaanee <heepan.kim@gmail.com>
2021-01-29 11:25:58 +0900

    04.02 Make  - "Create User Account" Query

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -9,4 +10,24 @@ export class UsersService {
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    try {
+      const exist = await this.usersRepository.findOne({ email });
+
+      if (exist) {
+        return 'There is a user with that email already';
+      }
+
+      await this.usersRepository.save(
+        this.usersRepository.create({ email, password, role }),
+      );
+    } catch (e) {
+      return "Couldn't create account";
+    }
+  }
 }
