6c954e601e18c2925bdb77462612b69dae570488
heepaanee <heepan.kim@gmail.com>
2021-02-23 15:20:09 +0900

    06.01 Add Email Verification State

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,12 +8,15 @@ import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { EditProfileInput } from './dtos/edit-profile.dto';
+import { Verification } from './entities/verification.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    @InjectRepository(Verification)
+    private readonly verificationsRepository: Repository<Verification>,
     private readonly config: ConfigService,
     private readonly jwtService: JwtService,
   ) {}
@@ -30,8 +33,12 @@ export class UsersService {
         return { ok: false, error: 'There is a user with that email already' };
       }
 
-      await this.usersRepository.save(
+      const user = await this.usersRepository.save(
         this.usersRepository.create({ email, password, role }),
+      );
+
+      await this.verificationsRepository.save(
+        this.verificationsRepository.create({ user }),
       );
 
       return { ok: true };
@@ -88,6 +95,11 @@ export class UsersService {
 
     if (email) {
       user.email = email;
+      user.verified = false;
+
+      await this.verificationsRepository.save(
+        this.verificationsRepository.create({ user }),
+      );
     }
 
     if (password) {
